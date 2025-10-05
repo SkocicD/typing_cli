215060d45fc2b66309f062d586967f25e0415e56
SkocicD <skocicdavid@gmail.com>
2025-10-05 14:48:58 -0400

    not working. Change printStrings to characters and have board be a 2d array

--- typing_cli.go
+++ typing_cli.go
@@ -14,249 +14,21 @@ type colorstruct struct{
     red string
     end string
 }
+type printString struct{
+    content string
+    color string
+}
+type boardRow struct{
+    segments []printString
+    changed bool
+}
 
-const alphlen int=26
-var alph [alphlen]string
 var colors colorstruct
 var cursorLine int
 var maxLine int
+var board []boardRow
 
-func declare_alph(){
-    alph[0]=
-`
-
-  __ _
- / _  |
-| (_| |
- \__,_|
-
-`
-    alph[1]=
-`  _     
- | |    
- | |__  
- | '_ \ 
- | |_) |
- |_.__/
-
-`
-    alph[2]=
-`
-
-   ___ 
-  / __|
- | (__ 
-  \___|
-
-`
-    alph[3]=
-`      _ 
-     | |
-   __| |
-  / _  |
- | (_| |
-  \__,_|
-
-`
-    alph[4]=
-`
-
-   ___ 
-  / _ \
- |  __/
-  \___|
-
-`
-    alph[5]=
-`   __ 
-  / _|
- | |_ 
- |  _|
- | |  
- |_|
-
-`
-    alph[6]=
-`
-
-   __ _ 
-  / _  |
- | (_| |
-  \__, |
-   __/ |
-  |___/`
-    alph[7]=
-`  _     
- | |    
- | |__  
- | '_ \ 
- | | | |
- |_| |_|
-
-`
-    alph[8]=
-`  _ 
- (_)
-  _ 
- | |
- | |
- |_|
-
-`
-    alph[9]=
-`    _ 
-   (_)
-    _ 
-   | |
-   | |
-   | |
-  _/ |
- |__/ `
-    alph[10]=
-`  _    
- | |   
- | | __
- | |/ /
- |   < 
- |_|\_\
-
-`
-    alph[11]=
-`  _ 
- | |
- | |
- | |
- | |
- |_|
-
-`
-    alph[12]=
-`
-
-  _ __ ___  
- | '_ ' _ \ 
- | | | | | |
- |_| |_| |_|
-
-`
-    alph[13]=
-`
-
-  _ __  
- | '_ \ 
- | | | |
- |_| |_|
-
-`
-    alph[14]=
-`
-
-   ___  
-  / _ \ 
- | (_) |
-  \___/ 
-
-`
-    alph[15]=
-`
-
-  _ __  
- | '_ \ 
- | |_) |
- | .__/ 
- | |    
- |_|`
-    alph[16]=
-`
-
-   __ _ 
-  / _  |
- | (_| |
-  \__, |
-     | |
-     |_|`
-    alph[17]=
-`
-
-  _ __ 
- | '__|
- | |   
- |_|
-
-`
-    alph[18]=
-`
-
-  ___ 
- / __|
- \__ \
- |___/
-
-`
-    alph[19]=
-`  _   
- | |  
- | |_ 
- | __|
- | |_ 
-  \__| 
-
-`
-    alph[20]=
-`
-
-  _   _ 
- | | | |
- | |_| |
-  \__,_|
-
-`
-    alph[21]=
-`
-
- __   __
- \ \ / /
-  \ V / 
-   \_/
-
-`
-    alph[22]=
-`
-
- __      __
- \ \ /\ / /
-  \ V  V / 
-   \_/\_/
-
-`
-    alph[23]=
-`
-
- __  __
- \ \/ /
-  >  < 
- /_/\_\
-
-`
-    alph[24]=
-`
-
-  _   _ 
- | | | |
- | |_| |
-  \__, |
-   __/ |
-  |___/`
-    alph[25]=
-`
-
-  ____
- |_  /
-  / / 
- /___|
-
-`
-}
+var lines = make(map[string]int)
 
 func streakString(streak int) string {
 
@@ -271,52 +43,62 @@ func streakString(streak int) string {
     return str
 }
 
-func moveCursorUp(numLines int){
-    if numLines>cursorLine{
-        numLines=cursorLine
+func updateLetter(letter string, color string){
+    var currLine = lines["question"]
+
+    letterLines := strings.Split(letter, "\n")
+
+    for i, q := range letterLines{
+        if len(board) == currLine + i {
+            newRow := boardRow{segments: []printString{}, changed: true}
+            board = append(board, newRow)
+        }
+        newSeg := printString{content: q, color: color}
+        board[currLine + i].segments = append(board[currLine + i].segments, newSeg)
     }
-    if numLines==0{
-        return
-    }
-    var s string
-    s=fmt.Sprintf("\033[%dA",numLines)
-    fmt.Print(s)
-    cursorLine-=numLines
 }
 
-func clearLines(numLines int){
-    i:=0
-    startingCursorLine:=cursorLine
-    for i < numLines && i < maxLine{
-        fmt.Print("\033[2K\r")
-        fmt.Print("\033[1B")
-        cursorLine+=1
-        i+=1
+func updateStreak(streak int){
+    var streakLine = lines["streak"]
+
+    for len(board) <= streakLine{
+        newRow := boardRow{segments: []printString{}, changed: true}
+        board = append(board,newRow)
     }
-    moveCursorUp(cursorLine-startingCursorLine)
+    newSeg := printString{content: fmt.Sprintf("Streak: %d",streak), color: colors.green}
+    board[streakLine].segments = append(board[streakLine].segments, newSeg)
 }
 
-func printAndCountLines(str string, color string, newLine bool){
-    if newLine{ str += "\n" }
-    cursorLine+=strings.Count(str,"\n")
-    fmt.Print(color)
-    fmt.Print(str)
-    fmt.Print(colors.end)
-    if cursorLine > maxLine{ maxLine=cursorLine }
+func refresh(){
+    for i := 0; i < len(board); i++ {
+        // if !board[i].changed { continue }
+
+        // clear the current row
+        fmt.Print("\r\033[2K")
+
+        // print the next row from board
+        for _, seg := range board[i].segments{
+            fmt.Print(seg.color)
+            fmt.Println(seg.content)
+        }
+    }
+
+    // always move the cursor back to the top
+    fmt.Print(fmt.Sprintf("\033[%dA",len(board) - 1))
 }
 
 func main() {
     colors=colorstruct{green: "\033[32m", end: "\033[0m", red: "\033[31m"}
     cursorLine=0
     maxLine=0
+    lines["question"] = 0
+    lines["streak"] = 8
 
     rand.Seed(time.Now().UnixNano())
-    declare_alph()
-    
+
     var question int
     var streak int
     streak=0
-    
 
     tty, err := tty.Open()
     if err != nil {
@@ -324,37 +106,31 @@ func main() {
     }
     defer tty.Close()
 
+    // infinite loop
     for {
-        question=rand.Intn(alphlen)
-        moveCursorUp(cursorLine)
-        clearLines(strings.Count(alph[question],"\n")+1)
-        printAndCountLines(alph[question],colors.end,true) // print the letter in default terminal color
-        fmt.Print(streakString(streak,true))
-        
+        question=rand.Intn(26)
+        updateLetter(alph[question], colors.green)
+        updateStreak(streak)
+        refresh()
+
         for{
             char, err := tty.ReadRune()
             if err != nil {
                 log.Fatal(err)
             }
+
             // Correct answer
             if int(char)-97 == question {
-                moveCursorUp(cursorLine)
-                clearLines(strings.Count(alph[question],"\n"))
-                printAndCountLines(alph[question],colors.green) 
-                time.Sleep(100*time.Millisecond)
+                // printAndCountLines(alph[question],colors.green) 
+                // time.Sleep(100*time.Millisecond)
                 streak+=1
                 break
-            }else{
-                moveCursorUp(cursorLine)
-                clearLines(strings.Count(alph[question],"\n")+1)
-                printAndCountLines(alph[question],colors.red) 
-                fmt.Print(streakString(streak,false))
-                time.Sleep(50*time.Millisecond)
-                moveCursorUp(cursorLine)
-                clearLines(strings.Count(alph[question],"\n")+1)
-                printAndCountLines(alph[question],colors.end) 
 
+            }else{
+                // time.Sleep(50*time.Millisecond)
                 streak=0
+                updateStreak(streak)
+                refresh()
             }
         }
     }
